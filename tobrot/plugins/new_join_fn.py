3a48c50e114cffd1908b2d90643ab320f7a16150
demonz00 <black0kid@gmail.com>
2020-04-05 07:44:16 +0700

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -35,7 +35,7 @@ async def new_join_f(client, message):
 async def help_message_f(client, message):
     # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
     channel_id = str(AUTH_CHANNEL)[4:]
-    message_id = 99
+    message_id = 27
     # display the /help message
     await message.reply_text(
         f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
@@ -48,7 +48,7 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/c/1388887326/42"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
