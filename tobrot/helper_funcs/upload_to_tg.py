178e2808b5884b37eefb7e3fd68dffe775a3f591
demonz00 <black0kid@gmail.com>
2020-04-05 13:36:28 +0700

    Update upload_to_tg.py

--- tobrot/helper_funcs/upload_to_tg.py
+++ tobrot/helper_funcs/upload_to_tg.py
@@ -72,7 +72,7 @@ async def upload_to_tg(
             i_m_s_g = await message.reply_text(
                 "Telegram does not support uploading this file.\n"
                 f"Detected File Size: {d_f_s} 😡\n"
-                "\n🤖 trying to split the files 🌝🌝🌚"
+                "\n🤖 Trying to split the files 🧩"
             )
             splitted_dir = await split_large_files(local_file_name)
             totlaa_sleif = os.listdir(splitted_dir)
@@ -83,7 +83,7 @@ async def upload_to_tg(
             await i_m_s_g.edit_text(
                 f"Detected File Size: {d_f_s} 😡\n"
                 f"<code>{ba_se_file_name}</code> splitted into {number_of_files} files.\n"
-                "trying to upload to Telegram, now ..."
+                "🧿 Trying to upload to Telegram, now ..."
             )
             for le_file in totlaa_sleif:
                 # recursion: will this FAIL somewhere?
@@ -178,7 +178,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    "trying to upload",
+                    "Trying to Upload ⏫",
                     message_for_progress_display,
                     start_time
                 )
@@ -250,7 +250,7 @@ async def upload_single_file(message, local_file_name, caption_str, from_user):
                 reply_to_message_id=message.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
                 progress_args=(
-                    "trying to upload",
+                    "Trying to Upload ⏫",
                     message_for_progress_display,
                     start_time
                 )
